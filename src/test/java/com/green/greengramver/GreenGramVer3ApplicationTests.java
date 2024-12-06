5272a4fe4e9d9dc7b117c1fc47d947fca5495766
SugiSagong <jumoney1012@gmail.com>
2024-12-06 15:47:07 +0900

    first

---
+++ src/test/java/com/green/greengramver/GreenGramVer3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.green.greengramver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenGramVer3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
