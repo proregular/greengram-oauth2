27019750c657e91adda002b6dad42c042c1e5f3c
SugiSagong <jumoney1012@gmail.com>
2024-12-10 10:48:07 +0900

    feat: 그린그램 ver3 24.12.10 실습 - 피드 삭제 기능 개발

--- src/main/java/com/green/greengramver/feed/FeedService.java
+++ src/main/java/com/green/greengramver/feed/FeedService.java
@@ -90,13 +90,17 @@ public class FeedService {
 
     @Transactional
     public int deleteFeed(FeedDeleteReq p) {
+        //피드 댓글, 좋아요 삭제
+        int affectedRowsEtc = feedMapper.delFeedLikeAndFeedCommentAndFeedPic(p);
+        log.info("affectedRows: {}", affectedRowsEtc);
+
         //피드 사진 삭제
         String deletePath = String.format("%s/feed/%d", myFileUtils.getUploadPath(), p.getFeedId());
         myFileUtils.deleteFolder(deletePath, true);
-        //피드 댓글, 좋아요 삭제
-        int affectedRows = feedMapper.delFeedLikeAndFeedCommentAndFeedPic(p);
-        log.info("affectedRows: {}", affectedRows);
+
         //피드 삭제
-        return feedMapper.delFeed(p);
+        int affectedRows = feedMapper.delFeed(p);
+
+        return 1;
     }
 }
